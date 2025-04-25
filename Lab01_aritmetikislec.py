b3de816e7390573d50b6db5594a2fd10578bbab3
Ahmet Demirtaş <ahmetdem1203@gmail.com>
2025-04-25 13:47:25 +0300

    Lab - Etkinlik 01
    Lab - Etkinlik 01

---
+++ Lab01_aritmetikislec.py
@@ -0,0 +1,9 @@
+sayi1 = int(input("Birinci sayıyı giriniz:"))
+sayi2 = int(input("İkinci sayıyı giriniz:"))
+print(sayi1,"+",sayi2,"=",sayi1+sayi2)
+print(sayi1,"-",sayi2,"=",sayi1-sayi2)
+print(sayi1,"x",sayi2,"=",sayi1*sayi2)
+print(sayi1,"üssü",sayi2,"=",sayi1**sayi2)
+print(sayi1,"/",sayi2,"=",sayi1/sayi2)  #Paydanın sıfır olmadığını varsayalım
+print(sayi1,"//",sayi2,"=",sayi1//sayi2) #Kalanlı bölümde bölüm
+print(sayi1,"MOD",sayi2,"=",sayi1%sayi2) #Kalanlı bölümde kalan
